aaf70e69cf8ab009fe588a94d8b509bee91a047b
shet1990 <shet1990@gmail.com>
2018-09-28 04:56:35 +0300

    sorter/decision

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.arr = [];
+        this.comparator = (a, b) => a - b;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.arr.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.arr.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.arr;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        const sortedArr = [];
+        const sortedIndices = indices.sort((a, b) => a - b);
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        for (let i = 0, len = sortedIndices.length; i < len; i++) {
+            sortedArr.push(this.arr[sortedIndices[i]]);
+        }
+
+        sortedArr.sort(this.comparator);
+
+        for (let i = 0, len = sortedIndices.length; i < len; i++) {
+            this.arr[sortedIndices[i]] = sortedArr.shift();
+        }
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
 }
 
 module.exports = Sorter;
